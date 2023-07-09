c727f22406ad423d14813583aab8da95185b181a
micah elm <micah.b.elm@gmail.com>
2023-07-08 20:24:19 -0700

    create the fetch to api

--- client/src/components/ListStudents.jsx
+++ client/src/components/ListStudents.jsx
@@ -1,6 +1,26 @@
-
+import { useState } from "react"
 export const ListStudents = () => {
+    const [students , setStudents] = useState([])
+
+    const getStudents = async () => {
+        try {
+          const response = await fetch("http://localhost:3000/api");
+          const jsonData = await response.json();
+    
+          setStudents(jsonData);
+        } catch (err) {
+          console.error(err.message);
+        }
+      };
+    
+      useEffect(() => {
+        getStudents();
+      }, []);
+    
+      console.log(students);
   return (
-    <div>ListStudents</div>
+    <>
+    
+    </>
   )
 }
