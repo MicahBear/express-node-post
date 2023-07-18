2ac1965c13aa4eb5f26fb6918648dd68b1fd9dd8
Sree2604 <ds04aranganthan@gmail.com>
2023-07-18 20:34:23 +0530

    created client directory and changes made

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { ListStudents } from './components/ListStudents'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <ListStudents />
+    </>
+  )
+}
+
+export default App
